c69bd1209a962956b6d84a7bc0e26901704463c0
Alex Cho <alexsjcho@gmail.com>
2019-01-31 15:38:31 -0800

    updated HTML, implemented some jQuery functions

--- scripts.js
+++ scripts.js
@@ -1,18 +1,92 @@
 "use strict";
 
-//USER STORY
-/* 
-1. New quiz question will have different image (5 images)
-2. Wrong answers will display a GIF (2 GIFs required)
-3. Start screen will have 1 background image, end screen will have different background image
-
-*/
-
 //All Questions Object
+const questionSet = [
+  {
+    number: 1,
+    text: `What is a string in JavaScript？"`,
+    ans1: `(a) a string that
+      you can tie your notes t`,
+    ans2: `(b) a string that
+      you can pluck and create musical soun`,
+    ans3: `(c) JavaScript
+      strings are used for storing and manipulating text`
+  },
+
+  {
+    number: 2,
+    text: `What does JavaScript?`,
+    ans1: `(a) an
+    object-oriented computer programming language commonly used to create
+    interactive effects within web browsers`,
+    ans2: `(b) It is an
+    extension of another programming langauge called "Java"`,
+    ans3: `(c) It's a type of
+    coffee brewery scripting art using Java beans`
+  },
+
+  {
+    number: 3,
+    text: `JavaScript can only be used on the frontend`,
+    ans1: `(a) True`,
+    ans2: `(b) False`
+  },
+  {
+    number: 4,
+    text: `The name JavaScript is borrowed from another programming language
+    "Java"`,
+    ans1: `(a) True`,
+    ans2: `(b) False`
+  },
+  {
+    number: 5,
+    text: `You cannot become a Web Developer just with JavaScript`,
+    ans1: `True`,
+    ans2: `False`
+  }
+];
 
 //All Answers Object
 
+//Global Variable declarations
+let questionNum = 1;
+let correctAnswers = 0;
+
 //Question Templates
+
+function questionTemplate(correctAnswers, question, questionsAnswered) {
+  return `
+      <section id="quiz-app" role="main">
+    <div class ="question-title">
+      <h2 id="question">${question.text}</h2>
+     </div> 
+      <form>
+        <fieldset>
+          <label>
+            <input class="answer" type="radio" name="option" checked></input>
+            <span>${question.ans1}</span>
+          </label>
+    
+          <label>
+            <input class="answer" type="radio" name="option"></input>
+            <span>${question.ans2}</span>
+          </label>
+    
+          <label>
+            <input class="answer" type="radio" name="option"></input>
+            <span>${question.ans3}</span>
+          </label>
+        </fieldset>  
+        <button id="js-submit-button">Submit</button>
+      </form>
+  
+      <div id="status-bar">
+        <span id="question-count">Question: ${question.number}/10</span>
+        <span id="score-count">Score: ${correctAnswers}/${questionsAnswered}</span>
+      </div>
+    </section>
+    `;
+}
 
 //Start Button function
 
@@ -32,5 +106,12 @@
 
 //Wrong answer feedback html
 
-//Wrong answer feedback UI tempalte
+//Wrong answer feedback UI template
 
+//Iterate through questions function
+
+//Correct answer iteration function
+
+//Function to handle button clicks (Recursion)
+
+//Call actionButton function
