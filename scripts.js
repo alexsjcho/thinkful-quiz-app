92972be55fd559cf322deb1d6987a3803a4f2557
Alex Cho <alexsjcho@gmail.com>
2019-01-30 17:39:45 -0800

    added user stories in script.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 "use strict";
+
+//USER STORY
+/* 
+1. New quiz question will have different image (5 images)
+2. Wrong answers will display a GIF (2 GIFs required)
+3. Start screen will have 1 background image, end screen will have different background image
+
+*/
+
